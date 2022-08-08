afd6847881dfe7dbc1c14dfc8d1b62c8f89ee131
Player - X <72035642+hasidu@users.noreply.github.com>
2022-08-08 23:54:47 +0200

    V1

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+description "Forza Horizon 4 Speedometer"
+author "Akkariin"
+url "https://www.zerodream.net/"
+
+ui_page "html/hud.html"
+
+files {
+	"html/hud.html",
+	"html/js/gauge.min.js",
+	"html/js/hud.js",
+	"html/css/hud.css",
+	"html/fonts/Oswald-Light.eot",
+	"html/fonts/Oswald-Light.svg",
+	"html/fonts/Oswald-Light.ttf",
+	"html/fonts/Oswald-Light.woff",
+	"html/fonts/Oswald-Light.woff2",
+	"html/fonts/Oswald-Regular.eot",
+	"html/fonts/Oswald-Regular.svg",
+	"html/fonts/Oswald-Regular.ttf",
+	"html/fonts/Oswald-Regular.woff",
+	"html/fonts/Oswald-Regular.woff2",
+	"html/images/speedcircle.png",
+}
+
+server_script {
+	'server.lua'
+}
+
+
+client_script "client.lua"
